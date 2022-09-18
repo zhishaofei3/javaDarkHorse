e2c597623a1021b722330e526845417a30f2ab4e
zhishaofei <zhishaofei@jd.com>
2022-09-18 15:18:06 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextByte();
+        System.out.println(number1);
+
+        int number2 = sc.nextInt();
+        System.out.println(number2);
+    }
+}
